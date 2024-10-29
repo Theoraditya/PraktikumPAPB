7601635433cbd51419b41f68b3a8cab22cdb2fd6
theoraditya <theoraditya195@gmail.com>
2024-10-29 20:33:47 +0700

    Materi Navigation

---
+++ Pertemuan7_NavigationFB/app/src/main/java/com/example/pertemuan8_roomdatabase/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.pertemuan8_roomdatabase
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.pertemuan8_roomdatabase.ui.theme.Pertemuan8_RoomDatabaseTheme
+import com.example.pertemuan8_roomdatabase.ui.theme.Pertemuan8_RoomDatabaseTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Pertemuan8_RoomDatabaseTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "login") {
+        composable("login") {
+            LoginUi(navController)
+        }
+        composable(
+            route = "home/{email}",
+            arguments = listOf(navArgument("email") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val email = backStackEntry.arguments?.getString("email")
+            HomeScreen(email)
+        }
+    }
+}
